89962f01887aa09084d4cf7af19ad6bba348fb4b
Ben Song <benedict04song@gmail.com>
2024-04-11 00:46:17 -0400

    Added testing suite capabilities as well as basic JSON-to-genetree code

--- src/evo/gtrees.py
+++ src/evo/gtrees.py
@@ -5,27 +5,65 @@ A *gene* is a namedtuple with two fields:
 - `.dna` contains the nucleotide sequence for this gene;
 - `.species` is the species ID associated with this gene.
 """
-from typing import NamedTuple, Self
+from typing import NamedTuple, Self, Optional, Any
+from collections import deque
+from dataclasses import dataclass
 import json
 
 class Gene(NamedTuple):
     dna: str
     species: int
 
+@dataclass
 class TreeNode:
-    left_child: Self
-    right_child: Self
-    value: Gene
+    left_child: Optional[Self]
+    right_child: Optional[Self]
+    value: Optional[Gene]
 
 class Tree:
     def __init__(self, json_str: str):
         """
         :param str json_str JSON string representing this gene tree's basic structure.
         """
-        self.root: Gene = None
+        self.root: Gene = TreeNode(None, None, None)
         self.raw: dict = json.loads(json_str)
-
+        print(self.raw)
+        # Initialize tree
         self._init_json()
 
     def _init_json(self):
-        pass
+        # dfs
+        def _dfs(node_to_fill: TreeNode, label: str, entry_in_json: Any):
+            # fill in node with content
+            val = label
+            species_val, dna_val = val.split(',', 2)
+            print(dna_val, species_val)
+            # assume structure of input JSON file
+            node_to_fill.value = Gene(dna=dna_val, species=species_val)
+            if len(entry_in_json) == 0:
+                # nothing to be done
+                return
+            elif len(entry_in_json) >= 1:
+                # recurse on left child
+                node_to_fill.left_child = TreeNode(None, None, None)
+                left = list(entry_in_json)[0]
+                _dfs(node_to_fill.left_child, left, entry_in_json[left])
+            if len(entry_in_json) == 2:
+                # then fill in the right child as well
+                node_to_fill.right_child = TreeNode(None, None, None)
+                right = list(entry_in_json)[1]
+                _dfs(node_to_fill.right_child, right, entry_in_json[right])
+        root_label = next(iter(self.raw))
+        _dfs(self.root, root_label, self.raw[root_label])
+    
+    def __repr__(self):
+        repres = ""
+        queue = deque()
+        queue.append((0, self.root))
+        while queue:
+            lvl, node = queue.popleft()
+            if node is not None:
+                repres += str(lvl) + " " + str(node.value) + "\n"
+                queue.append((lvl+1, node.left_child))
+                queue.append((lvl+1, node.right_child))
+        return repres
---
+++ src/runtests.py
@@ -0,0 +1,64 @@
+"""
+Simple testing suite.
+"""
+from typing import Any, Callable
+import evo.gtrees as gtrees
+import evo.strees as strees
+import os.path, sys
+import logging
+
+def run_test(test_no: int, test_fn: Callable[[str], Any], log: logging.Logger, iters: int = 1, against: Any = None) -> bool:
+    """
+    Runs test `test_no` on `test_fn` and compares it against `against`.
+    If `against` is not provided, then `run_test` will return true iff execution
+    finishes without errors. If `against` is provided, `run_test` will return the
+    result of __eq__ on the output of `test_fn` with `against`. All exceptions
+    are caught and logged to `log`, so as to not interrupt the test suite.
+    :param int test_no The test to run
+    :param Callable[[str], Any] test_fn The test function callable
+    :param logging.Logger Exceptions and status updates will be logged here
+    :param Any against The expected output of `test_fn` on `test_no`
+    :param int iters Number of times to run this test
+    """
+    test_input: str
+    test_path: str = os.path.join("src/tests", f"test{str(test_no).zfill(2)}.txt")
+    log.info(f"Running test #{test_no}: {test_path}")
+    # open test file and read in its contents
+    with open(test_path, "r") as tfile:
+        test_input = tfile.read()
+    result: Any
+    failure: bool = False
+    for iter in range(iters):
+        log.info(f"Iteration {iter}...")
+        try:
+            result = test_fn(test_input)
+        except Exception as e:
+            failure = True
+            logging.exception(repr(e))
+        if against is not None:
+            if result != against:
+                failure = True
+                logging.warning(f"Output:\n{result}\n" +
+                                f"does not match expected:\n{against}")
+            else:
+                logging.info("Output matched expected result!")
+    if failure:
+        logging.info("One or more iterations failed.")
+    else:
+        logging.info("Test case passed!")
+    return not failure
+
+def create_and_print_tree(j: str):
+    t = gtrees.Tree(j)
+    print(t)
+
+if __name__ == "__main__":
+    # setup
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('[%(levelname)s] %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    # run tests
+    run_test(1, create_and_print_tree, logger, 1)
